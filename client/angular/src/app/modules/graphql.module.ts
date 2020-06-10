79a065c981815f441b29b5d6905043e1d252c7a2
kozzzz-07 <64407592+kozzzz-07@users.noreply.github.com>
2020-06-11 01:34:18 +0900

    query mutation subscription の疎通 (local)

--- client/angular/src/app/modules/graphql.module.ts
+++ client/angular/src/app/modules/graphql.module.ts
@@ -1,24 +1,71 @@
 import { NgModule } from '@angular/core';
-import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
+import { ApolloModule, APOLLO_OPTIONS, Apollo } from 'apollo-angular';
 import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { WebSocketLink } from 'apollo-link-ws';
+import { split } from 'apollo-link';
+import { getMainDefinition } from 'apollo-utilities';
 
-const uri = 'http://localhost:8080/graphql'; // <-- add the URL of the GraphQL server here
-export function createApollo(httpLink: HttpLink) {
-  return {
-    link: httpLink.create({ uri }),
-    cache: new InMemoryCache(),
-  };
-}
+// const uri = 'http://localhost:8080/graphql'; // <-- add the URL of the GraphQL server here
+
+// const wsClient = new WebSocketLink({
+//   uri: `ws://localhost:8080/graphql`,
+//   options: {
+//     reconnect: true,
+//   },
+// });
+
+// export function createApollo(httpLink: HttpLink) {
+//   console.log(httpLink);
+//   return {
+//     link: httpLink.create({ uri }),
+//     cache: new InMemoryCache(),
+//   };
+// }
 
 @NgModule({
   exports: [ApolloModule, HttpLinkModule],
-  providers: [
-    {
-      provide: APOLLO_OPTIONS,
-      useFactory: createApollo,
-      deps: [HttpLink],
-    },
-  ],
+  // providers: [
+  //   {
+  //     provide: APOLLO_OPTIONS,
+  //     useFactory: createApollo,
+  //     deps: [HttpLink],
+  //   },
+  // ],
 })
-export class GraphQLModule {}
+export class GraphQLModule {
+  constructor(apollo: Apollo, httpLink: HttpLink) {
+    // Create an http link:
+    const http = httpLink.create({
+      uri: 'http://localhost:8080/graphql',
+    });
+
+    // Create a WebSocket link:
+    const ws = new WebSocketLink({
+      uri: `ws://localhost:8080/subscription`,
+      options: {
+        reconnect: true,
+      },
+    });
+
+    // using the ability to split links, you can send data to each link
+    // depending on what kind of operation is being sent
+    const link = split(
+      // split based on operation type
+      ({ query }) => {
+        const definition = getMainDefinition(query);
+        return (
+          definition.kind === 'OperationDefinition' &&
+          definition.operation === 'subscription'
+        );
+      },
+      ws,
+      http,
+    );
+
+    apollo.create({
+      link,
+      cache: new InMemoryCache(),
+    });
+  }
+}
